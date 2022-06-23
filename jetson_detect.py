6189dd2c4e7f50ec3c067f244fe07a6a88a8043d
AlexOliZ <alexandretomas@ua.pt>
2022-06-24 00:00:02 +0100

    update jetson stuff

--- jetson_detect.py
+++ jetson_detect.py
@@ -1,6 +1,7 @@
 import jetson.inference
 import jetson.utils
 
+import argparse
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
 
@@ -14,66 +15,67 @@ def send_message(msg):
     mySocket = socket( AF_INET, SOCK_DGRAM )
     mySocket.sendto(str.encode(msg),(ip,PORT_NUMBER))
     
-def main():
-    # parse the command line
-    parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.",
-    formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
-    jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 
-    parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
-    parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
-    parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
-    parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
-    parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use")
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.",
+formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 
-    is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use")
 
-    try:
-        opt = parser.parse_known_args()[0]
-    except:
-        print("")
-        parser.print_help()
-        sys.exit(0)
+is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
 
-    # create video output object 
-    output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
+try:
+    opt = parser.parse_known_args()[0]
+except:
+    print("")
+    parser.print_help()
+    sys.exit(0)
 
-    # load the object detection network
-    net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
+# create video output object 
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
 
-    # create video sources
-    input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+# load the object detection network
+net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 
+# create video sources
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 
-    # process frames until the user exits
-    while True:
-        # capture the next image
-        img = input.Capture()
+check = False
 
-        # detect objects in the image (with overlay)
-        detections = net.Detect(img, overlay=opt.overlay)
+# process frames until the user exits
+while True:
+    # capture the next image
+    img = input.Capture()
 
-        # print the detections
-        print("detected {:d} objects in image".format(len(detections)))
+    # detect objects in the image (with overlay)
+    detections = net.Detect(img, overlay=opt.overlay)
+    
+     # print the detections
+    print("detected {:d} objects in image".format(len(detections)))
 
-        for detection in detections:
-            send_message(str(detection))
+    for detection in detections:
+        send_message(str(detection))
+        check = True 
 
-            # render image
-            output.Render(img)
-            # update the title bar
-            output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
-            output.Render(img)
+        # render image
+        output.Render(img)
+        # update the title bar
+        output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+        output.Render(img)
 
-            # update the title bar
-            output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+        # update the title bar
+        output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
 
-            # print out performance info
-            net.PrintProfilerTimes()
+        # print out performance info
+        net.PrintProfilerTimes()
+        
+        # exit on input/output EOS
+        break
 
-            # exit on input/output EOS
-            if not input.IsStreaming() or not output.IsStreaming():
-                break
-
-if __name__ == "__main__":
-    main()
+    if not input.IsStreaming() or not output.IsStreaming() or check:
+        break
